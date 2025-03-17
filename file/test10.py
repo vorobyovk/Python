f94cb3851c2029b7487e6be8461886f397e9d4f5
Vorobyov Kirill <vorobyovk@gmail.com>
2025-03-17 09:47:38 +0100

    test10

--- file/test10.py
+++ file/test10.py
@@ -10,8 +10,25 @@ with open('file/test.txt', 'w') as f1:
 #         print(line)
 
 
-print(sys.argv)
-print(sys.path)
-print(sys.version) 
-print(sys.platform)
-print(sys.getfilesystemencoding())
+# print(sys.argv)
+# print(sys.path)
+# print(sys.version) 
+# print(sys.platform)
+# print(sys.getfilesystemencoding())
+
+# def nice_function():
+#     string = """Do nothing, but document it.
+#     No, really, it doesn't do anything.
+#     """    
+#     return string
+# def not_bad(s: str) -> str:
+#     if s.find("not") == -1 or s.find("bad") == -1: 
+#         print("not found")       
+#         return s
+#     else:
+#         print("found")
+#         return s.replace("not bad", "good")
+# # s= "This music is not not bad"        
+# #s = nice_function()
+# print(not_bad(s))
+
