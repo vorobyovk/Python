67a15742adfb1a9c28503819b95784b829985144
Vorobyov Kirill <vorobyovk@gmail.com>
2025-03-23 17:04:12 +0100

    lecture 7-8

--- Lecture 8/main.py
+++
@@ -1,7 +0,0 @@
-from decimal import Decimal
-
-print(0.1 + 0.2 == 0.3)
-print(Decimal("0.1") + Decimal("0.2") == Decimal("0.3"))
-
-print(0.1 + 0.2)
-print(Decimal("0.1") + Decimal("0.2"))
--- lecture 7 (steck, lifo, fifo)/main.py
+++ lecture 7 (steck, lifo, fifo)/main.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from collections import deque
+import decimal
+from decimal import Decimal, getcontext
 
 # words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
 # grouped_words = defaultdict(list)
@@ -71,24 +73,49 @@ from collections import deque
 # # Виведення поточного стану черги після видалення елементів
 # print("Черга після видалення елементів:", list(d))
 
-# Список завдань, де кожне завдання - це словник
-tasks = [
-    {"type": "fast", "name": "Помити посуд"},
-    {"type": "slow", "name": "Подивитись серіал"},
-    {"type": "fast", "name": "Вигуляти собаку"},
-    {"type": "slow", "name": "Почитати книгу"}
-]
-# Ініціалізація черги завдань
-task_queue = deque()
-# Розподіл завдань у чергу відповідно до їх пріоритету
-for task in tasks:
-    if task["type"] == "fast":
-        task_queue.appendleft(task)  # Додавання на високий пріоритет
-        print(f"Додано швидке завдання: {task['name']}")
-    else:
-        task_queue.append(task)  # Додавання на низький пріоритет
-        print(f"Додано повільне завдання: {task['name']}")
-# Виконання завдань
-while task_queue:
-    task = task_queue.popleft()
-    print(f"Виконується завдання: {task['name']}")
+# # Список завдань, де кожне завдання - це словник
+# tasks = [
+#     {"type": "fast", "name": "Помити посуд"},
+#     {"type": "slow", "name": "Подивитись серіал"},
+#     {"type": "fast", "name": "Вигуляти собаку"},
+#     {"type": "slow", "name": "Почитати книгу"}
+# ]
+# # Ініціалізація черги завдань
+# task_queue = deque()
+# # Розподіл завдань у чергу відповідно до їх пріоритету
+# for task in tasks:
+#     if task["type"] == "fast":
+#         task_queue.appendleft(task)  # Додавання на високий пріоритет
+#         print(f"Додано швидке завдання: {task['name']}")
+#     else:
+#         task_queue.append(task)  # Додавання на низький пріоритет
+#         print(f"Додано повільне завдання: {task['name']}")
+# # Виконання завдань
+# while task_queue:
+#     task = task_queue.popleft()
+#     print(f"Виконується завдання: {task['name']}")
+
+
+
+# print(0.1 + 0.2 == 0.3)
+# print(Decimal("0.1") + Decimal("0.2") == Decimal("0.3"))
+
+# print(0.1 + 0.2)
+# print(Decimal("0.1") + Decimal("0.2"))
+
+# print("===========================================")
+# getcontext().prec = 6
+# print(Decimal("1") / Decimal("7"))
+# getcontext().prec = 8
+# print(Decimal("1") / Decimal("7"))
+# number = Decimal("1.45")
+# # Округлення за замовчуванням до одного десяткового знаку
+# print("Округлення за замовчуванням ROUND_HALF_EVEN:", number.quantize(Decimal("0.0")))
+# # Округлення вверх при нічиї (ROUND_HALF_UP)
+# print("Округлення вгору ROUND_HALF_UP:", number.quantize(Decimal("0.0"), rounding=decimal.ROUND_HALF_UP))
+# # Округлення вниз (ROUND_FLOOR)
+# print("Округлення вниз ROUND_FLOOR:", number.quantize(Decimal("0.0"), rounding=decimal.ROUND_FLOOR))
+# # Округлення вверх (ROUND_CEILING)
+# print("Округлення вгору ROUND_CEILING:", number.quantize(Decimal("0.0"), rounding=decimal.ROUND_CEILING))
+# # Округлення до трьох десяткових знаків за замовчуванням
+# print("Округлення до трьох десяткових знаків:", Decimal("3.14159").quantize(Decimal("0.000")))
