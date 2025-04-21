d9fdc8cb2e05b8f12595850a21d9ae0f665151b3
Vorobyov Kirill <vorobyovk@gmail.com>
2025-04-21 10:41:13 +0200

    test

---
+++ Project/test.py
@@ -0,0 +1,23 @@
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        
+            
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        
